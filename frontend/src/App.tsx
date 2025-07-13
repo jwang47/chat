c01c358afc32aa1f056da7ce277e876e5378d718
jwang47 <jiahuawang7@gmail.com>
2025-07-13 12:18:22 -0700

    Integrate Radix UI theme provider.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,20 +4,23 @@ import { ChatInterface } from "@/pages/ChatInterface";
 import { Settings } from "./pages/Settings";
 import ComponentShowcase from "./pages/ComponentShowcase";
 import { ChatProvider } from "@/contexts/ChatContext";
+import { Theme } from "@radix-ui/themes";
 
 function App() {
   return (
-    <Router>
-      <ChatProvider>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<ChatInterface />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="components" element={<ComponentShowcase />} />
-          </Route>
-        </Routes>
-      </ChatProvider>
-    </Router>
+    <Theme>
+      <Router>
+        <ChatProvider>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<ChatInterface />} />
+              <Route path="settings" element={<Settings />} />
+              <Route path="components" element={<ComponentShowcase />} />
+            </Route>
+          </Routes>
+        </ChatProvider>
+      </Router>
+    </Theme>
   );
 }
 
